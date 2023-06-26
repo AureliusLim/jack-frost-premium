a5948e60f7070c2dff3b2edfeb60696400c3f78f
frncscantonio <francesca714.antonio@gmail.com>
2023-06-27 01:01:31 +0800

    Profile Information
    - organized structure
    - w/ button
    *side bar style still not implemented

--- src/routes/(pages)/profile/+page.svelte
+++ src/routes/(pages)/profile/+page.svelte
@@ -27,100 +27,42 @@
   };
 </script>
 
-<style>
-  .container {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-  }
-
-  .content-wrapper {
-    flex-grow: 1;
-    min-height: 200px;
-    overflow-y: auto;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-  }
-
-  .left-box {
-    width: 25%;
-    border: 1px solid black;
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .right-box {
-    margin: 0px 0px 0px 10px;
-    flex-grow: 1;
-    border: 1px solid black;
-    height: 500px;
-    padding: 10px;
-  }
-
-  .profile-wrapper {
-    margin: 20px;
-  }
-
-  h1 {
-    font-size: 24px;
-    font-weight: bold;
-  }
-
-  .label {
-    font-weight: bold;
-  }
-
-  .info-grid {
-    display: flex;
-    flex-wrap: wrap;
-    margin: 15px;
-  }
-
-  .info-item {
-    flex-basis: 50%;
-    padding: 5px;
-    
-  }
-</style>
-
 <svelte:head>
   <title>My Orders | Jack Frost Premium Ice Cream</title>
 </svelte:head>
 
-<div class="container">
-  <div class="content-wrapper">
-    <div class="left-box">
+<div class = "max-w-full flex flex-col min-h-min">
+  <div class="main-container">
+    <div class="side-menu">
       <button on:click={() => goto('/profile')}>Profile Information</button>
       <button>Change Password</button>
       <button on:click={() => goto('/myorders')}>My Orders</button>
       <button on:click={handleLogout}>Sign Out</button>
     </div>
 
-    <div class="right-box">
+    <div class="info-container">
       <div class="profile-wrapper">
         <h1>Profile Information</h1>
 
         <div class="info-grid">
           <div class="info-item">
-            <p class="label">First Name</p>
-            <p>{userdetails.firstName}</p>
+            <p>First Name</p>
+            <p class="detail">{userdetails.firstName}</p>
           </div>
 
           <div class="info-item">
-            <p class="label">Last Name</p>
-            <p>{userdetails.lastName}</p>
+            <p>Last Name</p>
+            <p class="detail">{userdetails.lastName}</p>
           </div>
 
           <div class="info-item">
-            <p class="label">Email Address</p>
-            <p>{userdetails.email}</p>
+            <p>Email Address</p>
+            <p class="detail">{userdetails.email}</p>
           </div>
 
           <div class="info-item">
-            <p class="label">Contact</p>
-            <p>{userdetails.contactNumber}</p>
+            <p>Contact</p>
+            <p class="detail">{userdetails.contactNumber}</p>
           </div>
           
         </div>
@@ -129,29 +71,109 @@
 
         <div class="info-grid">
           <div class="info-item">
-            <p class="label">Street</p>
-            <p>{userdetails.street}</p>
+            <p>Street</p>
+            <p class="detail">{userdetails.street}</p>
           </div>
 
           <div class="info-item">
-            <p class="label">City, Province</p>
-            <p>{userdetails.cityProvince}</p>
+            <p>City, Province</p>
+            <p class="detail">{userdetails.cityProvince}</p>
           </div>
         
-
           <div class="info-item">
-            <p class="label">Building/Floor/Unit Number</p>
-            <p>{userdetails.buildingNumber}</p>
+            <p>Building/Floor/Unit Number</p>
+            <p class="detail">{userdetails.buildingNumber}</p>
           </div>
 
           <div class="info-item">
-            <p class="label">Postal Code</p>
-            <p>{userdetails.postalCode}</p>
+            <p>Postal Code</p>
+            <p class="detail">{userdetails.postalCode}</p>
           </div>
-          
-
+        </div>
+        
+        <div class="button-container">
+          <button class="edit-button">Edit Details</button>
         </div>
       </div>
     </div>
   </div>
 </div>
+
+
+<style>
+  .main-container {
+    width: 1920px;
+    height: 720px;
+    display: flex;
+    gap: 40px;
+    justify-content: center;
+    padding: 40px;
+    background: #CDD5EB;
+  }
+
+  .side-menu {
+    width: 250px;
+    height: 423px;
+    padding-top: 20px;
+    display: flex;
+    flex-direction: column;
+
+    background: #FFF;
+    color: #383D55;
+    font-family: Istok Web;
+    font-weight: 700;
+  }
+
+  .info-container {
+    width: 1620px;
+    height: max;
+    border: 1px solid white;
+    padding: 10px;
+
+    background: #FFF;
+    color: #383D55;
+    font-family: Istok Web;
+    font-weight: 700;
+  }
+
+  .profile-wrapper {
+    padding: 30px;
+    font-size: 17px;
+  }
+
+  h1 {
+    font-size: 29px;
+    font-weight: bold;
+  }
+
+  .info-grid {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .info-item {
+    margin-bottom: 40px;
+    flex-basis: 50%;
+  }
+  
+  .detail {
+    font-weight: 400;
+  }
+
+ .button-container {
+    display: flex;
+    align-items: right;
+    justify-content: right;
+    margin-top: 30px;
+  }
+
+  .edit-button {
+    width: 180px;
+    padding: 5px;
+    border-radius: 20px;
+    background: #383D55;
+    color: #FFF;
+  }
+</style>

2cc749be322516a474b6a31a0f2b858986c03a52
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2023-07-07 17:36:27 +0800

    listen for clicks to toggle searchresults

--- src/lib/components/Inputs/Searchbar.svelte
+++ src/lib/components/Inputs/Searchbar.svelte
@@ -1,9 +1,11 @@
 <script lang='ts'>
   import { debounce } from 'lodash';
-  import { onMount } from 'svelte';
-  import { createEventDispatcher } from 'svelte'; 
+  import { onMount, afterUpdate } from 'svelte';
+  import { createEventDispatcher } from 'svelte';
+
   let searchTerm = '';
   let fullName = [];
+  let showSearchResults = false;
   export let searchResults = []; // need to import the whole product list
   export let fullList;
 
@@ -37,16 +39,36 @@
     debouncedSearch();
   };
 
+  const listenOnClick = () =>{
+    showSearchResults = true;
+  }
+
   const handleSearchClick = () => {
     dispatch('search', searchTerm); // Dispatch 'search' event with the search term
   };
   const handleResultClick = (result) =>{
     dispatch('search', result)
   }
+  // Hide search results when clicking outside the search container
+  const handleClickOutside = (event) => {
+    const searchContainer = document.querySelector('.input-box');
+    if (searchContainer && !searchContainer.contains(event.target)) {
+      showSearchResults = false;
+    }
+  }
+  
+  // Listen for clicks on the document
+  afterUpdate(() => {
+    if (showSearchResults) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+  });
 </script>
 
 <div class='search-container'>
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
+   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <span class="search-icon" on:click={handleSearchClick}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -54,11 +76,11 @@
     
   </span>
 
-  <input type="text" placeholder="Search..." bind:value={searchTerm} on:input={handleSearchInput} />
+  <input class="input-box" type="text" placeholder="Search..." bind:value={searchTerm} on:input={handleSearchInput} on:click={listenOnClick}/>
 
   
 </div>
-{#if searchResults.length > 0}
+{#if searchResults.length > 0 && showSearchResults}
   <div class="search-results">
 
       <ul>

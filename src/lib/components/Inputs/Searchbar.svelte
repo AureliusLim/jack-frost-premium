485fa372a30cb036973f548a932e149b9aff0c1b
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2023-07-07 13:52:57 +0800

    implement search function

---
+++ src/lib/components/Inputs/Searchbar.svelte
@@ -0,0 +1,116 @@
+<script lang='ts'>
+  import { debounce } from 'lodash';
+  import { onMount } from 'svelte';
+  import { createEventDispatcher } from 'svelte'; 
+  let searchTerm = '';
+  let fullName = [];
+  export let searchResults = []; // need to import the whole product list
+  export let fullList;
+
+
+  const dispatch = createEventDispatcher();
+  onMount(async()=>{
+    console.log('Mounting search')
+    for(let i = 0; i < fullList.length; i++){
+      fullName[i] = fullList[i].name;
+    }
+
+  })
+  const search = async () => {
+    // Simulating asynchronous behavior with a delay
+    await new Promise(resolve => setTimeout(resolve, 500));
+    
+    if(searchTerm){
+      searchResults = fullName.filter(item => item.toLowerCase().includes(searchTerm.toLowerCase()))
+    }
+  };
+
+  // Debounce the search function to reduce API calls
+  const debouncedSearch = debounce(search, 300);
+
+  // Handler for search input changes
+  const handleSearchInput = (event) => {
+    searchTerm = event.target.value;
+    if (!searchTerm) {
+      searchResults = []; // Reset search results when search term becomes empty
+    }
+    debouncedSearch();
+  };
+
+  const handleSearchClick = () => {
+    dispatch('search', searchTerm); // Dispatch 'search' event with the search term
+  };
+  const handleResultClick = (result) =>{
+    dispatch('search', result)
+  }
+</script>
+
+<div class='search-container'>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <span class="search-icon" on:click={handleSearchClick}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+    </svg>
+    
+  </span>
+
+  <input type="text" placeholder="Search..." bind:value={searchTerm} on:input={handleSearchInput} />
+
+  
+</div>
+{#if searchResults.length > 0}
+  <div class="search-results">
+
+      <ul>
+        {#each searchResults as result}
+        <li><button on:click={handleResultClick(result)}>{result}</button></li>
+        {/each}
+      </ul>
+      
+
+  </div>
+{/if}
+
+<style>
+  .search-container {
+    display: flex;
+    align-items: center;
+    width: 30%;
+    margin-bottom: 10px;
+  }
+  .search-results {
+    position: absolute;
+    z-index: 1; 
+    background-color: white;
+    padding: 5px;
+    margin-top: 5px;
+    margin-left:25px;
+    width: 30%; 
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);
+  }
+  .search-results button {
+    width: 100%; /* Occupy the entire width of the container */
+    text-align: left; 
+    padding: 0.5rem; 
+  }
+
+  input {
+    flex-grow: 1;
+    padding: 8px;
+    font-size: 16px;
+    border: 1px solid black;
+    margin-left:10px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    margin: 4px 0;
+  }
+  .search-icon{
+    cursor: pointer;
+  }
+</style>
--- src/routes/(pages)/menu/+page.svelte
+++ src/routes/(pages)/menu/+page.svelte
@@ -4,7 +4,9 @@
 	import ProductInfo from '$lib/components/Modal/ProductInfo.svelte';
 	import type { Product } from '$lib/types/product';
 	import type { Offering } from '$lib/types/offering';
-
+	import Searchbar from '$lib/components/Inputs/Searchbar.svelte';
+	let searchTerm = "";
+	let searchResults = [];
 	const showProductInfo = async (event) => {
 		productInfoModal = true;
 		let res = await fetch(`/api/products/${event.detail.productId}`);
@@ -20,6 +22,13 @@
 		product = {} as Product;
 		pId = '';
 	};
+	const handleSearch = (event) =>{
+		searchTerm = event.detail;
+		searchResults = data.products.filter((product) =>
+    	product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  	);
+		console.log(searchResults)
+	}
 
 	let pId = '';
 	let productInfoModal = false;
@@ -31,25 +40,43 @@
 <svelte:head>
 	<title>Menu | Jack Frost Premium Ice Cream</title>
 </svelte:head>
+<div class="searchbar-container">
+  <Searchbar fullList={data.products} on:search={handleSearch}></Searchbar>
+</div>
 
+{#if !searchTerm}
+	<div class="product-list-container">
+		<ProductList
+			width="w-full md:max-w-[90%]"
+			products={data.products}
+			on:showItem={showProductInfo}
+		/>
+	</div>
+{:else}
 <div class="product-list-container">
 	<ProductList
 		width="w-full md:max-w-[90%]"
-		products={data.products}
+		products={searchResults}
 		on:showItem={showProductInfo}
 	/>
 </div>
+{/if}
 
 {#if productInfoModal}
 	<ProductInfo {product} {offerings} on:closeProductInfo={closeProductInfo} />
 {/if}
 
 <style lang="postcss">
+
+	 .searchbar-container {
+    margin-bottom: 20px;
+    height: 60px; /* Adjust the height as needed */
+  }
 	.product-list-container {
 		@apply w-full bg-white flex flex-col justify-center items-center;
 	}
 
-	@media sceen and (min-width: 768px) {
+	@media screen and (min-width: 768px) {
 		.product-list-container {
 			@apply flex-row;
 		}
